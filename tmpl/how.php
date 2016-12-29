686fb4f48ca7bd6816236b6269efd50a77d67392
nickydt4work@gmail.com <nickydt4work@gmail.com>
2016-12-29 13:56:06 +0500

    show 3

--- tmpl/how.php
+++ tmpl/how.php
@@ -4,14 +4,55 @@
 	</h3>
 
 	<ol class="no-list counter-start clear f-14 upper bold">
-<?		for($i=0;$i<7;++$i): ?>
 		<li class="w-25-pc w-50-pc-sm pull-left relative onmiddle h-90 m-b-40 circle-bg bg-left bg-auto-100pc bg-no-repeat valign-middle-hack ws-nowrap"><?
 			?><div class="absolute left top bottom w-90 f-66 counter-get-after text-center p-t-5 opac-20"></div><?
 			?><div class="w-170 m-l-35 inblock ws-normal">
 				Принимаем вашу заявку
 			</div><?
 		?></li>
-<?		endfor; ?>
+
+		<li class="w-25-pc w-50-pc-sm pull-left relative onmiddle h-90 m-b-40 circle-bg bg-left bg-auto-100pc bg-no-repeat valign-middle-hack ws-nowrap"><?
+			?><div class="absolute left top bottom w-90 f-66 counter-get-after text-center p-t-5 opac-20"></div><?
+			?><div class="w-170 m-l-35 inblock ws-normal">
+				Выявляем Вашу потребность
+			</div><?
+		?></li>
+
+		<li class="w-25-pc w-50-pc-sm pull-left relative onmiddle h-90 m-b-40 circle-bg bg-left bg-auto-100pc bg-no-repeat valign-middle-hack ws-nowrap"><?
+			?><div class="absolute left top bottom w-90 f-66 counter-get-after text-center p-t-5 opac-20"></div><?
+			?><div class="w-170 m-l-35 inblock ws-normal">
+				Находим лучшее решение задачи
+			</div><?
+		?></li>
+
+		<li class="w-25-pc w-50-pc-sm pull-left relative onmiddle h-90 m-b-40 circle-bg bg-left bg-auto-100pc bg-no-repeat valign-middle-hack ws-nowrap"><?
+			?><div class="absolute left top bottom w-90 f-66 counter-get-after text-center p-t-5 opac-20"></div><?
+			?><div class="w-170 m-l-35 inblock ws-normal">
+				Заключаем<br>с вами договор
+			</div><?
+		?></li>
+
+		<li class="w-25-pc w-50-pc-sm pull-left relative onmiddle h-90 m-b-40 circle-bg bg-left bg-auto-100pc bg-no-repeat valign-middle-hack ws-nowrap"><?
+			?><div class="absolute left top bottom w-90 f-66 counter-get-after text-center p-t-5 opac-20"></div><?
+			?><div class="w-170 m-l-35 inblock ws-normal">
+				выполняем необходимые&nbsp;задачи
+			</div><?
+		?></li>
+
+		<li class="w-25-pc w-50-pc-sm pull-left relative onmiddle h-90 m-b-40 circle-bg bg-left bg-auto-100pc bg-no-repeat valign-middle-hack ws-nowrap"><?
+			?><div class="absolute left top bottom w-90 f-66 counter-get-after text-center p-t-5 opac-20"></div><?
+			?><div class="w-170 m-l-35 inblock ws-normal">
+				Согласовываем с вами результат
+			</div><?
+		?></li>
+
+		<li class="w-25-pc w-50-pc-sm pull-left relative onmiddle h-90 m-b-40 circle-bg bg-left bg-auto-100pc bg-no-repeat valign-middle-hack ws-nowrap"><?
+			?><div class="absolute left top bottom w-90 f-66 counter-get-after text-center p-t-5 opac-20"></div><?
+			?><div class="w-170 m-l-35 inblock ws-normal">
+				Оказываем поддержку
+			</div><?
+		?></li>
+
 		<li class="w-25-pc w-50-pc-sm pull-left relative onmiddle h-90 m-b-40">
 			<img src="assets/img/mark.png" class="w-155 border-box brd-r-1">
 		</li>
